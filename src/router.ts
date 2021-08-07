bf0358d1b8d6033c109d3ec611c8aa84617d2c44
Jean Junior <jclgjunior@gmail.com>
2021-08-06 23:55:42 -0300

    Send message to rabbit queue

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import RabbibtmqServer from './services/rabbitmq-server';
 
 const router = Router();
 
 router.get('/', (req, res) => {
   return res.json({ message: 'Hello world With Docker Change' });
+});
+
+router.get('/rabbit', async (req, res) => {
+  const server = new RabbibtmqServer('amqp://admin:admin@rabbitmq:5672');
+  await server.start();
+  await server.publishInQueue('teste', JSON.stringify(req.query));
+  return res.json({ message: 'Message sent to queue' });
 });
 
 export default router;
